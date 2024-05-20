a470038cfbe5bb677d3c1a81a78f67e7bc195e89
jerry <160117974+15571601@users.noreply.github.com>
2024-05-20 14:21:19 +0900

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,5 @@
+<!-- Bootstrap core JavaScript -->
+    <script src="/static/js/jquery.min.js" nonce=a9becc2ef47cd00d1a654bd308df0524></script>
+    <script src="/static/js/bootstrap.min.js" nonce=a9becc2ef47cd00d1a654bd308df0524></script> 
+</body>
+</html>
